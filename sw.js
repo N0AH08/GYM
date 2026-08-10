f4f5a98e87619d2890fa845140cd76319dce638c
mockly-website <mockly.website@gmail.com>
2026-08-10 12:13:18 +0200

    Ridisegno app: 6 pagine separate, menu in basso, schede M/A/B aggiornate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gym-scheda-v5';
+const CACHE = 'gym-scheda-v6';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
